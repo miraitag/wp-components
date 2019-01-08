01a1ad3bd71e909f8b35a7ce17d268d5e5cf7d1e
Oscar A. Ruiz Tahuilán <oruiz@anzen.com.mx>
2019-01-08 17:43:51 -0600

    UPDATE: mixin, recursos, postcss-env

--- src/components.js
+++ src/components.js
@@ -1,4 +1,58 @@
-import './components.pcss'
+import './components/configuration.pcss';
+import './components/variables.pcss';
+/* ATOMS */
+import './components/atoms/cbx-bullet-list/style.pcss';
+import './components/atoms/cbx-button/style.pcss';
+import './components/atoms/cbx-button-boolean/style.pcss';
+import './components/atoms/cbx-category/style.pcss';
+import './components/atoms/cbx-headline-hero/style.pcss';
+import './components/atoms/cbx-headline-section/style.pcss';
+import './components/atoms/cbx-hightlight/style.pcss';
+import './components/atoms/cbx-icon/style.pcss';
+import './components/atoms/cbx-img/style.pcss';
+import './components/atoms/cbx-legal/style.pcss';
+import './components/atoms/cbx-link/style.pcss';
+import './components/atoms/cbx-list/style.pcss';
+import './components/atoms/cbx-sub-category/style.pcss';
+import './components/atoms/cbx-sub-headline/style.pcss';
+import './components/atoms/cbx-sub-text/style.pcss';
+import './components/atoms/cbx-sub-title/style.pcss';
+import './components/atoms/cbx-text/style.pcss';
+import './components/atoms/cbx-title/style.pcss';
+
+/* CONTAINERS */
+import './components/containers/comparador/style.pcss';
+
+/* MOLECULES */
+import './components/molecules/cbx-apps/style.pcss';
+import './components/molecules/cbx-apps-promo/style.pcss';
+import './components/molecules/cbx-btn-back/style.pcss';
+import './components/molecules/cbx-card/style.pcss';
+import './components/molecules/cbx-card-list/style.pcss';
+import './components/molecules/cbx-check/style.pcss';
+import './components/molecules/cbx-compare-modal/style.pcss';
+import './components/molecules/cbx-cookies/style.pcss';
+import './components/molecules/cbx-credit-card/style.pcss';
+import './components/molecules/cbx-economia/style.pcss';
+import './components/molecules/cbx-exit/style.pcss';
+import './components/molecules/cbx-faqs/style.pcss';
+import './components/molecules/cbx-footer/style.pcss';
+import './components/molecules/cbx-grid-eyf/style.pcss';
+import './components/molecules/cbx-header/style.pcss';
+import './components/molecules/cbx-hero/style.pcss';
+import './components/molecules/cbx-info/style.pcss';
+import './components/molecules/cbx-list-square/style.pcss';
+import './components/molecules/cbx-modal/style.pcss';
+import './components/molecules/cbx-perfilador/style.pcss';
+import './components/molecules/cbx-promociones/style.pcss';
+import './components/molecules/cbx-side-menu/style.pcss';
+import './components/molecules/cbx-slick/style.pcss';
+import './components/molecules/cbx-social-info/style.pcss';
+import './components/molecules/cbx-steps/style.pcss';
+import './components/molecules/cbx-submenu/style.pcss';
+import './components/molecules/cbx-tabs/style.pcss';
+import './components/molecules/cbx-tip/style.pcss';
+import './components/molecules/cbx-wrapper/style.pcss';
 
 class Hero{
     constructor(name){
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,14 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import postcssPresetEnv from 'postcss-preset-env'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import fs from 'fs'
 
 export default (env, argv) => {
     //console.log(`El modo es ${argv.mode} y es verdero? ${ argv.mode === 'development'}`)
     return {
-        watch: true,
+		watch: true,
         entry: {
-            'cbx-components': './src/components.js'
+			'cbx-components': ['core-js/modules/es6.promise','core-js/modules/es6.array.iterator','./src/components.js']
         },
         output: {
             path: `${__dirname}/dist/`,
@@ -32,10 +33,39 @@ export default (env, argv) => {
                             options: {
                                 importLoaders: 1
                             }
-                        },
+						},
                         {
-                            loader: 'postcss-loader'
-                        }
+							loader: 'postcss-loader', 
+							options: {
+								ident: 'postcss',
+								parser: 'postcss',
+								plugins: () => [
+									require('postcss-import')(),
+									require('postcss-mixins')(),
+									require('postcss-conditionals')(),
+									/* require('postcss-media-variables')(), */
+									//require('postcss-css-variables')(),
+									require('postcss-nested-props')(),
+									postcssPresetEnv({
+										stage: 1,
+										features: {
+											'nesting-rules': true,
+											'custom-media-queries': true,
+											'custom-properties': true,
+											':not-pseudo-class': true,
+											':matches-pseudo-class': true,
+											'media-query-ranges': true
+										},
+										browsers: 'IE 11',
+										autoprefixer: { grid: true }
+									}),
+									require('cssnano')()
+									/* require('postcss-advanced-variables')(), */
+									/* require('postcss-simple-vars')(), */
+									/* require('postcss-at-rules-variables')(), */
+								]
+							}
+						}
                     ]
                 }
             ]
@@ -45,6 +75,10 @@ export default (env, argv) => {
                 filename: "[name].css",
                 chunkFilename: "[id].css"
             })
-        ]
-    }
+        ],
+		target: 'node',
+		node: {
+			global: true
+		}
+	}
 }
